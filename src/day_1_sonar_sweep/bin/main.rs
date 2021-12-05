db28ddc9acdd19bedc5d381c6a6179db4418ba6e
gas <gasparian>
2021-12-06 02:33:15 +0500

    day 1; got 1 star

--- src/day_1_sonar_sweep/bin/main.rs
+++ src/day_1_sonar_sweep/bin/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-	println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+	if let Ok(lines) = read_lines(&args[1]) {
+		let mut counter: i32 = 0;
+		let mut prev: i32 = -1;
+		for (i, line) in lines.enumerate() {
+			if let Ok(l) = line {
+				let val = l.parse::<i32>().unwrap();
+				if i == 0 {
+					prev = val;
+					continue;
+				}
+				if val > prev {
+					counter += 1;
+				}
+				prev = val;
+			}
+		}
+		println!("{}", counter);
+	}
+}
+
+fn read_lines(fname: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
+	let file = File::open(fname)?;
+	Ok(io::BufReader::new(file).lines())
 }
