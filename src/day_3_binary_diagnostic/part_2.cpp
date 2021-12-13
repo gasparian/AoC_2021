49059cb6fac7349ab43215343cac7eeff3038795
gas <gasparian>
2021-12-13 13:08:17 +0500

    day 3 sol. function splitted

--- src/day_3_binary_diagnostic/part_2.cpp
+++ src/day_3_binary_diagnostic/part_2.cpp
@@ -2,30 +2,44 @@
 
 using namespace std;
 
+struct stats {
+    uint32_t counter;
+    vector<uint32_t> starts_one_ids;
+    vector<uint32_t> starts_zero_ids;
+};
+
+stats get_data_stats(vector<int>& data, uint32_t bit_id) {
+	uint32_t counter = 0;
+    uint32_t is_non_zero;
+    vector<uint32_t> starts_one_ids, starts_zero_ids;
+    for (uint32_t i=0; i < data.size(); i++) {
+        is_non_zero = data[i] & (1 << bit_id);
+        if (is_non_zero) {
+            starts_one_ids.push_back(i);
+            counter++;
+        } else {
+            starts_zero_ids.push_back(i);
+        }
+    }
+    return {
+        counter,
+        starts_one_ids,
+        starts_zero_ids,
+    };
+}
+
 uint32_t get_ox(vector<int>& data, uint32_t bit_id) {
     if (data.size() == 1) {
         return data[0];
     }
-	uint32_t counter = 0;
-    uint32_t is_non_zero;
-    vector<uint32_t> start_one_ids, start_zero_ids;
-    for (uint32_t i=0; i < data.size(); i++) {
-        is_non_zero = data[i] & (1 << bit_id);
-        if (is_non_zero) {
-            start_one_ids.push_back(i);
-            counter++;
-        } else {
-            start_zero_ids.push_back(i);
-        }
-    }
-    const uint32_t half_size = data.size() / 2;
+    stats data_stats = get_data_stats(data, bit_id);
     vector<int> sub_set;
-    if (counter >= (data.size() - counter)) {
-        for (auto i : start_one_ids) {
+    if (data_stats.counter >= (data.size() - data_stats.counter)) {
+        for (auto i : data_stats.starts_one_ids) {
             sub_set.push_back(data[i]);
         }
     } else {
-        for (auto i : start_zero_ids) {
+        for (auto i : data_stats.starts_zero_ids) {
             sub_set.push_back(data[i]);
         }
     }
@@ -36,26 +50,14 @@ uint32_t get_co2(vector<int>& data, uint32_t bit_id) {
     if (data.size() == 1) {
         return data[0];
     }
-	uint32_t counter = 0;
-    uint32_t is_non_zero;
-    vector<uint32_t> start_one_ids, start_zero_ids;
-    for (uint32_t i=0; i < data.size(); i++) {
-        is_non_zero = data[i] & (1 << bit_id);
-        if (is_non_zero) {
-            start_one_ids.push_back(i);
-            counter++;
-        } else {
-            start_zero_ids.push_back(i);
-        }
-    }
-    const uint32_t half_size = data.size() / 2;
+    stats data_stats = get_data_stats(data, bit_id);
     vector<int> sub_set;
-    if (counter >= (data.size() - counter)) {
-        for (auto i : start_zero_ids) {
+    if (data_stats.counter >= (data.size() - data_stats.counter)) {
+        for (auto i : data_stats.starts_zero_ids) {
             sub_set.push_back(data[i]);
         }
     } else {
-        for (auto i : start_one_ids) {
+        for (auto i : data_stats.starts_one_ids) {
             sub_set.push_back(data[i]);
         }
     }
@@ -68,10 +70,9 @@ uint64_t get_life_support_rating(ifstream* input, uint32_t bit_len) {
     while (getline(*input, line)) {
 		data.push_back(stoi(line, 0, 2));
     }
-    uint32_t ox = get_ox(data, bit_len - 1);
-    uint32_t co2 = get_co2(data, bit_len - 1);
-    uint64_t life_support_rating = ox * co2;
-    return life_support_rating;
+    const uint32_t ox = get_ox(data, bit_len - 1);
+    const uint32_t co2 = get_co2(data, bit_len - 1);
+    return ox * co2;
 }
 
 int main(int argc, char** argv) {
@@ -82,8 +83,8 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    uint32_t bit_len = 12;
-    uint64_t life_support_rating = get_life_support_rating(&input, bit_len);
+    const uint32_t bit_len = 12;
+    const uint64_t life_support_rating = get_life_support_rating(&input, bit_len);
     cout << life_support_rating << endl; // 5941884
     input.close();
 }
